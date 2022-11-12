a29611633d89dc51db471bc1e174fe18bef662b7
rajshekar11 <rajshekar767@gmail.com>
2022-11-12 22:22:05 +0530

    raj part

--- TripMaker/src/main/java/com/tripmaker/exception/GlobalExceptionHandler.java
+++ TripMaker/src/main/java/com/tripmaker/exception/GlobalExceptionHandler.java
@@ -111,7 +111,25 @@ public class GlobalExceptionHandler {
 		
 	}
 	
-	
+	@ExceptionHandler(BusException.class)
+	public ResponseEntity<MyErrorDetails> busExceptionHandler(BusException be,WebRequest req){
+		MyErrorDetails myE=new MyErrorDetails();
+		myE.setTimestamp(LocalDateTime.now());
+		myE.setMessage(be.getMessage());
+		myE.setDescription(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(myE,HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(HotelException.class)
+	public ResponseEntity<MyErrorDetails> hotelExceptionHandler(HotelException he,WebRequest req){
+		MyErrorDetails myE=new MyErrorDetails();
+		myE.setTimestamp(LocalDateTime.now());
+		myE.setMessage(he.getMessage());
+		myE.setDescription(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(myE,HttpStatus.BAD_REQUEST);
+	}
 	
 	
 }
