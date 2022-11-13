df6746e4126b9f4d7e034870ef60c96dd9e26da9
Shashwat Srivastava <62950509+JavaDevShashwat@users.noreply.github.com>
2022-11-13 16:43:23 +0530

    Update TravelService.java

--- TripMaker/src/main/java/com/tripmaker/service/TravelService.java
+++ TripMaker/src/main/java/com/tripmaker/service/TravelService.java
@@ -2,6 +2,8 @@ package com.tripmaker.service;
 
 
 
+import java.util.List;
+
 import com.tripmaker.exception.TravelsException;
 import com.tripmaker.model.Travels;
 
@@ -10,6 +12,6 @@ public interface TravelService {
 
 	public Travels addTravels(Travels travel , String key) throws TravelsException;
 	public Travels updateTravel(Travels travel , String key) throws TravelsException;
-	public String removeTravel(Travels travel , String key) throws TravelsException;
-	public Travels searchTravels(Integer travelId, String key) throws TravelsException;
+	public String removeTravel(Integer travelId, String key) throws TravelsException;
+	public List<Travels> searchTravels(String key) throws TravelsException;
 }
